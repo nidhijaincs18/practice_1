ebbb15d2647e20263bd641f258394902fe632221
nidhijaincs18 <nidhi.jain_cs18@gla.ac.in>
2019-10-01 22:35:56 +0530

    creating a method to calculate index of ch

--- src/main/Question10.java
+++ src/main/Question10.java
@@ -4,22 +4,42 @@ import java.util.Scanner;
 
 public class Question10 {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);\
+        String str = s.nextLine();
         char ch = s.next().charAt(0);
+
     }
 }
 
 class Ques8 {
     private char ch;
+    private String str;
 
     public Ques8(char ch) {
         this.ch = ch;
+        this.str = str;
     }
 
-    public char getCH() {
+    public char getCh() {
         return ch;
     }
 
+    public void setCh(char ch) {
+        this.ch = ch;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public int index() {
+        int index1 = getStr().indexOf(getCh());
+        return index1;
+    }
 
 
 }
