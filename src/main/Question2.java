ba0f85fcc3e0d73fde9097fc0817a5c17b448824
nidhijaincs18 <nidhi.jain_cs18@gla.ac.in>
2019-10-01 21:45:05 +0530

    creating setter methods

--- src/main/Question2.java
+++ src/main/Question2.java
@@ -26,4 +26,12 @@ class Ques2 {
     public int getIndex() {
         return index;
     }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
 }
