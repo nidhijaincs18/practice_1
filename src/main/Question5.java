31f375e9dc00af7c412039d03107fc2d88a17a69
nidhijaincs18 <nidhi.jain_cs18@gla.ac.in>
2019-10-01 21:57:19 +0530

    taking an input using Scanner Class

--- src/main/Question5.java
+++ src/main/Question5.java
@@ -1,7 +1,11 @@
 package main;
 
+import java.util.Scanner;
+
 public class Question5 {
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        String substr = s.nextLine();
     }
 }
