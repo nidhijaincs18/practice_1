f2599d2fadfb66234e09b20dad6a5ef98d723b45
nidhijaincs18 <nidhi.jain_cs18@gla.ac.in>
2019-10-01 21:49:32 +0530

    init commit

--- src/main/Question2.java
+++ src/main/Question2.java
@@ -5,12 +5,11 @@ import java.util.Scanner;
 public class Question2 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int index = Integer.parseInt(s.nextLine());
+
         String str = s.nextLine();
+        int index = Integer.parseInt(s.nextLine());
         Ques2 q = new Ques2(str, index);
         System.out.println(q.subStr());
-
-
     }
 }
 
