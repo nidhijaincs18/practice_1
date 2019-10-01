dc8780620089282589b57821f15296e6954f4051
nidhijaincs18 <nidhi.jain_cs18@gla.ac.in>
2019-10-01 21:42:04 +0530

    giving an input statement using Scanner class

--- src/main/Question2.java
+++ src/main/Question2.java
@@ -1,7 +1,11 @@
 package main;
 
+import java.util.Scanner;
+
 public class Question2 {
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(System.in);
+        int index = Integer.parseInt(s.nextLine());
+        String str = s.nextLine();
     }
 }
