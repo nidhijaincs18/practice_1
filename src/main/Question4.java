f0a81d7b4e3f37e2cc100f5e07bfd450fe3cfd2b
nidhijaincs18 <nidhi.jain_cs18@gla.ac.in>
2019-10-02 15:17:14 +0530

    creating a method to replace vowels with special characters

--- src/main/Question4.java
+++ src/main/Question4.java
@@ -6,6 +6,7 @@ public class Question4 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String str = s.nextLine();
+
     }
 }
 
@@ -23,4 +24,26 @@ class Ques4 {
     public void setStr(String str) {
         this.str = str;
     }
+
+    public String vowels() {
+        String s1 = getStr();
+        String s2 = "*^!";
+        int temp = 0;
+        for (int i = 0; i < getStr().length(); i++) {
+            char c = getStr().charAt(i);
+            if (temp < 3) {
+                if (c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I' || c == 'O' || c == 'o' || c == 'U' || c == 'u') {
+                    s1 = s1.replace(getStr().charAt(i), s2.charAt(temp));
+                    temp += 1;
+                }
+            } else {
+                temp = 0;
+                i--;
+            }
+
+        }
+        return s1;
+    }
+
+
 }
