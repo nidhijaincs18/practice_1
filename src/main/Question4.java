8fafc5951ba9610994da4e168802892a5f6c2d18
nidhijaincs18 <nidhi.jain_cs18@gla.ac.in>
2019-10-02 15:35:02 +0530

    modifies the method vowels()

--- src/main/Question4.java
+++ src/main/Question4.java
@@ -27,15 +27,17 @@ class Ques4 {
     }
 
     public String vowels() {
-        String s1 = getStr();
+        StringBuilder sb = new StringBuilder();
         String s2 = "*^!";
         int temp = 0;
         for (int i = 0; i < getStr().length(); i++) {
             char c = getStr().charAt(i);
             if (temp < 3) {
                 if (c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I' || c == 'O' || c == 'o' || c == 'U' || c == 'u') {
-                    s1 = s1.replace(getStr().charAt(i), s2.charAt(temp));
+                    sb.append(s2.charAt(temp));
                     temp += 1;
+                } else {
+                    sb.append(c);
                 }
             } else {
                 temp = 0;
@@ -43,7 +45,7 @@ class Ques4 {
             }
 
         }
-        return s1;
+        return sb.toString();
     }
 
 
